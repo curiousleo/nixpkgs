9fc419e6a3d069afbfd0463c86a0dabac12a9e23
Leonhard Markert <curiousleo@users.noreply.github.com>
2019-11-07 10:39:12 +0100

    full updater approach: abort!

--- pkgs/tools/misc/lorri/default.nix
+++ pkgs/tools/misc/lorri/default.nix
@@ -11,7 +11,16 @@
 , cf-private
 }:
 
-rustPlatform.buildRustPackage rec {
+let
+  src = fetchFromGitHub {
+    owner = "target";
+    repo = pname;
+    # Run `eval $(nix-build -A lorri.updater)` after updating the revision!
+    # See passthru.update below for what this does.
+    rev = "03f10395943449b1fc5026d3386ab8c94c520ee3";
+    sha256 = "0fcl79ndaziwd8d74mk1lsijz34p2inn64b4b4am3wsyk184brzq";
+  };
+
   pname = "lorri";
   version = "rolling-release-2019-10-30";
 
@@ -22,31 +31,27 @@ rustPlatform.buildRustPackage rec {
     maintainers = [ maintainers.Profpatsch ];
   };
 
-  src = fetchFromGitHub {
-    owner = "target";
-    repo = pname;
-    # Run `eval $(nix-build -A lorri.updater)` after updating the revision!
-    # See passthru.update below for what this does.
-    rev = "03f10395943449b1fc5026d3386ab8c94c520ee3";
-    sha256 = "0fcl79ndaziwd8d74mk1lsijz34p2inn64b4b4am3wsyk184brzq";
-  };
 
-  passthru.updater = with builtins; writeScript "copy-runtime-nix.sh" ''
+in
+  import ./default-upstream.nix { inherit pkgs src; } // {
+    updater = with builtins; writeScript "copy-runtime-nix.sh" ''
     #!${runtimeShell}
-    cp ${src}/default.nix ${toString ./default-upstream.nix}
-    cp ${src}/nix/runtime.nix ${toString ./runtime.nix}
-    cp ${src}/nix/runtime-closure.nix.template ${toString ./runtime-closure.nix.template}
-  '';
+      cp ${src}/default.nix ${toString ./default-upstream.nix}
+      cp ${src}/Cargo.nix ${toString ./Cargo.nix}
+      cp ${src}/nix/crates-io.nix ${toString ./crates-io.nix}
+      cp ${src}/nix/runtime.nix ${toString ./runtime.nix}
+      cp ${src}/nix/runtime-closure.nix.template ${toString ./runtime-closure.nix.template}
+    '';
+  }
 
-  BUILD_REV_COUNT = src.revCount or 1;
-  RUN_TIME_CLOSURE = pkgs.callPackage ./runtime.nix {};
+  #BUILD_REV_COUNT = src.revCount or 1;
+  #RUN_TIME_CLOSURE = pkgs.callPackage ./runtime.nix {};
 
-  nativeBuildInputs = [ pkgs.nix pkgs.direnv pkgs.which ];
-  buildInputs =
-    stdenv.lib.optionals stdenv.isDarwin [ CoreServices Security cf-private ];
+  #nativeBuildInputs = [ pkgs.nix pkgs.direnv pkgs.which ];
+  #buildInputs =
+    #stdenv.lib.optionals stdenv.isDarwin [ CoreServices Security cf-private ];
 
-  cargoSha256 = "1daff4plh7hwclfp21hkx4fiflh9r80y2c7k2sd3zm4lmpy0jpfz";
+  #cargoSha256 = "1daff4plh7hwclfp21hkx4fiflh9r80y2c7k2sd3zm4lmpy0jpfz";
 
   # Note that https://travis-ci.org/target/lorri/builds/605036891 passed.
-  doCheck = false;
-}
+  #doCheck = false;
