dad85f194787d3fe9168c1121eac199ebe07da49
Leonhard Markert <curiousleo@users.noreply.github.com>
2019-11-08 13:27:18 +0100

    Reset

--- pkgs/tools/misc/lorri/default.nix
+++ pkgs/tools/misc/lorri/default.nix
@@ -19,34 +19,31 @@ rustPlatform.buildRustPackage rec {
     description = "Your project's nix-env";
     homepage = "https://github.com/target/lorri";
     license = licenses.asl20;
-    maintainers = [ maintainers.Profpatsch ];
+    maintainers = with maintainers; [ grahamc Profpatsch ];
   };
 
   src = fetchFromGitHub {
     owner = "target";
     repo = pname;
-    # Run `eval $(nix-build -A lorri.updater)` after updating the revision!
+    # Run `eval $(nix-build -A lorri.updateScript)` after updating the revision!
     # See passthru.update below for what this does.
     rev = "03f10395943449b1fc5026d3386ab8c94c520ee3";
     sha256 = "0fcl79ndaziwd8d74mk1lsijz34p2inn64b4b4am3wsyk184brzq";
   };
 
-  patches = [ ./ignore-integration-tests.patch ];
-  doCheck = true;
-  cargoSha256 = "1daff4plh7hwclfp21hkx4fiflh9r80y2c7k2sd3zm4lmpy0jpfz";
-
-  BUILD_REV_COUNT = src.revCount or 1;
-  RUN_TIME_CLOSURE = pkgs.callPackage ./runtime.nix {};
-
-  passthru.updater = with builtins; writeScript "copy-runtime-nix.sh" ''
+  passthru.updateScript = with builtins; writeScript "copy-runtime-nix.sh" ''
     #!${runtimeShell}
-    set -euo pipefail
-
     cp ${src}/nix/runtime.nix ${toString ./runtime.nix}
     cp ${src}/nix/runtime-closure.nix.template ${toString ./runtime-closure.nix.template}
   '';
 
+  BUILD_REV_COUNT = src.revCount or 1;
+  RUN_TIME_CLOSURE = pkgs.callPackage ./runtime.nix {};
+
   nativeBuildInputs = [ pkgs.nix pkgs.direnv pkgs.which ];
   buildInputs =
     stdenv.lib.optionals stdenv.isDarwin [ CoreServices Security cf-private ];
+
+  cargoSha256 = "1daff4plh7hwclfp21hkx4fiflh9r80y2c7k2sd3zm4lmpy0jpfz";
+  doCheck = false;
 }
